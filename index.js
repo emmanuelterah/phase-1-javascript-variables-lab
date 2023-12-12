e5523b0cb57cefdf43681bec96880d6a5b62d6c5
emmanuelterah <emmanuelterah98@gmail.com>
2023-12-12 16:21:24 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution in this file!
+let companyName = 'Scuber';
+
+
+function theCompanyName () {
+const companyName = "Scuber";
+return companyName;
+}
+
+let mostProfitableNeighborhood = "Chelsea"
+function neighborhood() {
+    mostProfitableNeighborhood = "Chelsea";
+    return mostProfitableNeighborhood;
+}
+
+let companyCeo = "Susan Smith"
+
+function theCompanyCEO() {
+    companyCeo = "Susan Smith";
+    return companyCeo;
+}
+
